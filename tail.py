5028ada9cac572602f1d7785c3c9e8bd2ef228d7
Nick Lazaga <nicklazaga@berkeley.edu>
2021-09-16 11:35:57 -0700

    added tail.py

---
+++ tail.py
@@ -0,0 +1,154 @@
+#!/usr/bin/python
+
+import os
+import sys
+import argparse
+import time
+import signal
+from threading import Thread, Lock, Event
+from pathlib import Path
+
+
+class Tail(object):
+    def __init__(self, fnames, n):
+        self.files = [f for f in [File(fn) for fn in fnames] if f.is_valid()]
+        self.n = n
+        self.BLOCK_SIZE = 8192
+        self._lock = Lock()
+
+    def tails(self):
+        """Print header and last N lines of file.
+        
+        Only print header if given multiple files to read."""
+        for i, f in enumerate(self.files):
+            if len(self.files) > 1: print(f"\n==> {f.get_filename()} <==")
+            self._lastNLines(f, self.n)
+
+    def _lastNLines(self, file, n):
+        """Print last N lines of file and update filesize."""
+        with open(file.get_filename(), "r") as f:
+            newlines = 0
+            pos = filesize = f.seek(0, os.SEEK_END)
+            while pos > 0 and newlines < n + 1:
+                if pos < self.BLOCK_SIZE: curr_block_size = pos
+                else: curr_block_size = self.BLOCK_SIZE
+                pos -= curr_block_size
+                f.seek(pos)
+                block = f.read(curr_block_size)
+                newlines += block.count('\n')
+                if newlines >= n + 1:
+                    nlList = [i for i, c in enumerate(block) if c == '\n']
+                    diff = n + 1 - newlines
+                    offset = nlList[-diff] + 1
+                    f.seek(pos + offset)
+            if newlines < n + 1:
+                f.seek(0)
+            while (f.tell() < filesize):
+                print(f.read(self.BLOCK_SIZE), end='')
+            file.update_pos(filesize)
+
+    def followFile(self, file):
+        with self._lock:
+            n = 1
+            with open(file.get_filename(), "r") as f:
+                curr_end = f.seek(0, os.SEEK_END)
+                diff = curr_end - file.get_pos()
+                if curr_end - file.get_pos() < 0:
+                    print(f"{file.get_filename()}: file truncated")
+                    f.seek(0)
+                else:
+                    f.seek(file.get_pos())
+                if f.tell() < curr_end:
+                    print(f"\n==> {file.get_filename()} <==")
+                while f.tell() < curr_end:
+                    print(f.read(self.BLOCK_SIZE), end='')
+                file.update_pos(curr_end)
+
+
+class Follower(Thread):
+    def __init__(self, target, args):
+        Thread.__init__(self)
+        self.shutdown_flag = Event()
+        self.target = target
+        self.args = args
+    
+    def run(self):
+        while not self.shutdown_flag.is_set():
+            self.target(self.args)
+            time.sleep(0.1)
+
+
+class File(object):
+    def __init__(self, filename, pos=0):
+        self.filename = filename
+        self.pos = pos
+
+    def get_filename(self):
+        return self.filename
+    
+    def get_pos(self):
+        return self.pos
+
+    def update_pos(self, pos):
+        self.pos = pos
+
+    def is_valid(self):
+        file = Path(self.get_filename())
+        if not file.exists():
+            print(f"tail: cannot open '{self.get_filename()}': " +
+                  "No such file or directory")
+            return False
+        if file.is_dir():
+            print(f"tail: cannot open '{self.get_filename()}': " +
+                  "Is a directory")
+            return False
+        return True
+
+
+class ServiceExit(Exception):
+    """For clean exit of follower threads"""
+    pass
+
+
+def signal_handler(signum, frame):
+    raise ServiceExit
+
+def main():
+    parser = argparse.ArgumentParser(description='''Print the last 10 lines of
+                                     each FILE to standard output. With more
+                                     than one FILE, precede each with a header
+                                     giving the file name.''')
+
+    parser.add_argument("-n", "--lines", type=int, metavar="NUM",
+                        help="output last NUM lines, instead of last 10")
+
+    parser.add_argument("FILE", nargs=argparse.ONE_OR_MORE,
+                        help="name of file")
+
+    parser.add_argument("-f", "--follow", action="store_true",
+                        help="output appended data as the file grows")
+
+    args = parser.parse_args()
+    if args.lines == None: args.lines = 10
+    if args.lines < 0: args.lines = abs(args.lines)
+    t = Tail(args.FILE, args.lines)
+    t.tails()
+    if not args.follow: return
+    threads = []
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+    try:
+        for f in t.files:
+            follower = Follower(target=t.followFile, args=f)
+            threads.append(follower)
+            follower.start()
+        while True:
+            time.sleep(0.1)
+    except ServiceExit:
+        for t in threads:
+            t.shutdown_flag.set()
+            t.join()
+        print()
+
+if __name__ == '__main__':
+    main()
